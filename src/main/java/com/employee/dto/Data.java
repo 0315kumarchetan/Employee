5eb0aa04fac6b14a07ccf855d8c3d94b1bd4657b
0315kumarchetan <0315kumarchetan@gmail.com>
2022-12-09 15:54:50 +0530

    [chetan]|addComment

--- src/main/java/com/employee/dto/Data.java
+++ src/main/java/com/employee/dto/Data.java
@@ -1,7 +1,7 @@
 package com.employee.dto;
 
 public class Data {
-
+	//chnage is here
 	public static final String FAILED="failed";
 	public static final String SUCCESS="success";
 	public static final String EXISTS="Employee exists already";
